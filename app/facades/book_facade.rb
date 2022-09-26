6fae556c5a40a5741518761d41f382d7ff91ee98
Blake Saylor <Blakesaylor@gmail.com>
2022-09-26 11:33:53 -0600

    Basic formatting

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,14 +1,13 @@
 class BookFacade
   def self.get_books(title, quantity)
     all_books_data = BookService.book_details(title)
+    total_hits = all_books_data[:numFound]
+    index_end = quantity.to_i - 1
+
     output = Hash.new
     output[:books] = []
-
-    total_hits = all_books_data[:numFound]
     output[:total_hits] = total_hits
 
-    index_end = quantity.to_i - 1
-  
     all_books_data[:docs][0..index_end].each do |book_data|
       output[:books] << Book.new(book_data)
     end
