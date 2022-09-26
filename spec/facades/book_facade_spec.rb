e0d52212e0c7973f04d904ebb64b53c94adbbd04
Blake Saylor <Blakesaylor@gmail.com>
2022-09-26 11:17:00 -0600

    Add test for total book count

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -1,33 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'BookFacade', :vcr do
-  it 'makes a call and returns a hash with Books data' do
-    title = "denver,co"
-    quantity = '5'
-    books = BookFacade.get_books(title, quantity)
+  describe '#get_books' do
+    it 'makes a call and returns a hash with specified number of Books data' do
+      title = "denver,co"
+      quantity = '5'
+      books = BookFacade.get_books(title, quantity)
 
-    expect(books).to be_a Array
+      expect(books).to be_a Array
 
-    books.each do |book|
-      expect(book).to be_a Book
+      books.each do |book|
+        expect(book).to be_a Book
 
-      expect(book.title).to be_a String
+        expect(book.title).to be_a String
 
-      if book.isbn.is_a?(Array)
-        book.isbn.each do |isbn|
-          expect(isbn).to be_a String
+        if book.isbn.is_a?(Array)
+          book.isbn.each do |isbn|
+            expect(isbn).to be_a String
+          end
+        else
+          expect(book.isbn).to be_a String
         end
-      else
-        expect(book.isbn).to be_a String
-      end
 
-      if book.publisher.is_a?(Array)
-        book.publisher.each do |publisher|
-          expect(publisher).to be_a String
+        if book.publisher.is_a?(Array)
+          book.publisher.each do |publisher|
+            expect(publisher).to be_a String
+          end
+        else
+          expect(book.publisher).to be_a String
         end
-      else
-        expect(book.publisher).to be_a String
       end
+    end
+  end
+
+  describe '#get_total_book_count' do
+    it 'returns the total count of books for a given search term' do
+      title = "denver,co"
+      total_books = BookFacade.get_total_book_count(title)
+      expect(total_books).to eq 38
     end
   end
 end
